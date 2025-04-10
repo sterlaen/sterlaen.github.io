b8a6c4c502ebbfb48b96d792865fd5a90487c0d6
chris <kaplonn7@gmail.com>
2025-04-10 09:54:27 +0800

    Update simple-calculator.py

--- simple-calculator.py
+++ simple-calculator.py
@@ -1,23 +1,26 @@
 def calculator(): 
     print("simple calculator")
-print("operations: +,-,*,/")
+    print("operations: +,-,*,/")
 
-num1 = float(input('enter first number:')) #asks for an input and converts it from a string to a number
-op = input("enter operator:") #takes the operation as a string
-num2 = float(input('enter second number:')) #asks for an input and converts it from a string to a number
+    try:
+        num1 = float(input('enter first number:')) #asks for an input and converts it from a string to a number
+        op = input("enter operator:") #takes the operation as a string
+        num2 = float(input('enter second number:')) #asks for an input and converts it from a string to a number
 
-if op == "+":
-    print(f"result: {num1 + num2}") #+
-elif op == "-":
-    print(f"result: {num1 - num2}") #-
-elif op == "*":
-    print(f"result: {num1 * num2}") #*
-elif op == "/":
-    if num2 != 0: # checks if the second number is not zero
-        print(f"result: {num1 / num2}")
-    else:
-        print("error")
-else:
-    print("invalid operator") # if the operator is not one of the above, it will print invalid operator
-
+        if op == "+":
+            print(f"result: {num1 + num2}") #+
+        elif op == "-":
+            print(f"result: {num1 - num2}") #-
+        elif op == "*":
+            print(f"result: {num1 * num2}") #*
+        elif op == "/":
+            if num2 != 0: # checks if the second number is not zero
+               print(f"result: {num1 / num2}")
+            else:
+                print("dividion by 0 is unallowed")
+        else:
+            print("invalid operator") # if the operator is not one of the above, it will print invalid operator
+    except ValueError:
+        print("invalid input")
+        
 calculator() # calls the calculator function
