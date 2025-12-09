a81d65815d759f9070b651ffad615ae47499a015
sterlaen <kaplonn7@gmail.com>
2025-12-09 17:49:09 +0800

    Enhance training.html with animated text and additional image; add training.js for console logging examples

---
+++ bbcs/training.js
@@ -0,0 +1,13 @@
+console.log("bbcs"); // outputs whatever is in the paranthesis to the console,string
+console.log(2025); // integer
+console.log(true); // boolean
+
+let variable = "BuildingBloCS";
+console.log(typeof variable); // string
+
+variable = 2025;
+console.log(typeof variable); // number // reassigning variable, cannot start with "let", can change type
+
+variable = true;
+console.log(typeof variable); // boolean
+
